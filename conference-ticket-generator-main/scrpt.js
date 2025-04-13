2d32ac1bb45c9d14a3f8b359f41260fa81abc6b3
Roodri02 <rodrigocabellohernandez07@gmail.com>
2025-04-13 21:14:15 +0200

    First page of ticket-generator finished, but is not responsive yet

--- conference-ticket-generator-main/scrpt.js
+++ conference-ticket-generator-main/scrpt.js
@@ -1,11 +1,31 @@
+// VARIABLES FOR THE CODE
+
 const uploadInput = document.querySelector(".upload-avatar");
 const uploadImage = document.querySelector(".drag-and-drop-figure img");
 const uploadParagraph = document.querySelector(".drag-and-drop-p");
 const sendButton = document.querySelector(".main-form");
+const buttonsContainer = document.querySelector(".upload-avatar-buttons-container");
 
+// 500KB
 const maxSize = 500 * 1024; //500 KB
 
+// Function called when upload and new image or change the actual image failed.
+const failedImageSelected = (file)=>{
+    uploadImage.setAttribute("src",`assets/images/icon-upload.svg`);
+    uploadImage.style.width = "60%";
+    uploadParagraph.classList.remove("hidden");
+    buttonsContainer.classList.add("hidden");
+    uploadInput.style.pointerEvents = "all";
+    file.value="";
+}
 
+
+// Adding event listeners to the remove and change buttons
+buttonsContainer.lastElementChild.addEventListener('click',()=>{
+    uploadInput.click();
+})
+
+//Adding callbacks to the upload input element
 uploadInput.addEventListener("change",() => {
     if(uploadInput != undefined){
         const archivo = this.file;
@@ -16,16 +36,19 @@ uploadInput.addEventListener("change",() => {
         }
         else{
             if(archivo && uploadImage && archivo.value!=="" ){
-
                 let ruta = archivo.value.split("\\");
                 uploadImage.setAttribute("src",`assets/images/${ruta[ruta.length-1]}`);
                 uploadImage.style.width = "100%";
                 uploadParagraph.classList.add("hidden");
+                buttonsContainer.classList.remove("hidden");
+                uploadInput.style.pointerEvents = "none";
+                uploadInput.style.userSelect = "none";
+                buttonsContainer.firstElementChild.addEventListener('click',()=>{
+                    failedImageSelected(archivo);
+                })
             }
             else {
-                uploadImage.setAttribute("src",`assets/images/icon-upload.svg`);
-                uploadImage.style.width = "60%";
-                uploadParagraph.classList.remove("hidden");
+                failedImageSelected(archivo);
             }
         }
     }
